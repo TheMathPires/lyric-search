f0010c98ed44038d153b95933585e9b1c96ee2de
Matheus Holstein Pires <themathpires@gmail.com>
2021-10-26 09:07:01 +0100

    lyric search api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { Wrapper, Input, Button, Title, Lyrics } from './styles';
+import axios from "axios";
 
 function App() {
+  const artistInput = React.createRef();
+  const musicInput = React.createRef();
+  const [lyricsArea, setLyric] = useState('Search for your favorite lyric!');
+
+  const api = axios.create ({
+    baseURL: 'https://api.lyrics.ovh/v1'
+  })
+
+  const getLyrics = async ({artist, music}) => {
+    const {data} = await api.get(`/${artist}/${music}`);
+    
+    data.lyrics.split("\n").map(function(item) { 
+      console.log(item);
+    })
+    
+    return data.lyrics;
+  }
+
+  const handleOnSearch = async () => {
+    const lyrics =  await getLyrics({artist: artistInput.current.value, music: musicInput.current.value});
+
+    setLyric(lyrics);
+    return lyrics;
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Lyric Search</Title>
+      <Input type="text" placeholder="Artist" ref={artistInput}/>
+      <Input type="text" placeholder="Music" ref={musicInput}/> 
+      <br/><br/>
+      <Button type="submit" onClick={handleOnSearch}>Search</Button>
+      <hr/>
+      <Lyrics>{lyricsArea}</Lyrics>
+    </Wrapper>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+    text-align: center;
+    max-width: 990px;
+    margin: 0 auto;
+`
+
+export const Input = styled.input`
+    margin-right: 1rem;
+    padding: .5rem;
+    border-radius: 6px;
+`
+
+export const Button = styled.button`
+    padding: .5rem;
+    border-radius: 6px;
+    width: 150px;
+    margin-bottom: 1rem;
+    &:hover {
+        cursor: pointer;
+        color: #fff;
+        background-color: #2c2c2c;
+    }
+`
+
+export const Title = styled.h1`
+    font-size: 2rem;
+`
+export const Lyrics = styled.div`
+    margin-top: 1rem;
+    max-width: 
+`
